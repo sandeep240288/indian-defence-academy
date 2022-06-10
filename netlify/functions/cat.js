97281e3b3cb0ee340026db8c0238117255228afb
Sandeep Kumar Yadav <sandeep240288@gmail.com>
2022-06-11 01:36:23 +0530

    Ok

--- netlify/functions/cat.js
+++ netlify/functions/cat.js
@@ -1,14 +1,18 @@
 import axios from 'axios'
 const API_ENDPOINT = 'https://cat-fact.herokuapp.com/facts';
+
+console.log('Outside Handler');
 exports.handler = async (event, context) => {
     let data1 =[];
+    console.log(API_ENDPOINT);
     axios.get(API_ENDPOINT,{
         headers: {
                 "Accept": "application/json",
         }
     }).then(function (response) {
-       data1 = response.data;
+       data1 = JSON.stringify(response.data);
        console.log(data1);
+       console.log('inside axios');
 
     })
     .catch(function (error) {
