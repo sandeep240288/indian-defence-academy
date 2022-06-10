9fc620f036e7eaafee0593ee4e90b184dc5d3ef9
Sandeep Kumar Yadav <sandeep240288@gmail.com>
2022-06-11 01:14:33 +0530

    cat js

--- netlify/functions/cat.js
+++ netlify/functions/cat.js
@@ -1,18 +1,16 @@
-// const nodeFetch = require('node-fetch') as typeof fetch;
-
-// import fetch from 'node-fetch';
 import axios from 'axios'
 
-const API_ENDPOINT = 'https://cat-fact.herokuapp.com/facts';
 exports.handler = async (event, context) => {
-    const result = axios.get(API_ENDPOINT)
+    let data =[];
+    axios.get(API_ENDPOINT)
     .then(function (response) {
-        return JSON.stringify(response.data)
+       data = response.data;
     })
     .catch(function (error) {
         return {
-        statusCode: 422,
-        body: `Error: ${error}`
+            statusCode: 422,
+            body: `Error: ${error}`
         }
-   });
+    });
+    return data;
 }
