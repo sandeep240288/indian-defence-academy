e9378805192622bb320adc186b1fd046f477aa19
Sandeep Kumar Yadav <sandeep240288@gmail.com>
2022-06-11 02:17:00 +0530

    cat

--- netlify/functions/articles.js
+++ netlify/functions/articles.js
@@ -1,19 +1,15 @@
 import axios from 'axios'
-// const API_ENDPOINT = 'https://cat-fact.herokuapp.com/facts';
-
 const options ={ 
   baseURL: "https://whispering-stream-89457.herokuapp.com" ,
   headers:{ 
-          token:'ZRfWX6ScXAb69l36Fre7sTNm4FHhZaq5mJX010vP',
-          "Accept": "application/json",
+    token:process.env.MY_API_KEY,
+    "Accept": "application/json",
   } 
 };
 
 console.log('Outside Handler');
 exports.handler = async (event, context) => {
-    console.log('Inside handler');
     return axios.get('/api/articles',options).then(function (response) {
-        console.log('inside axios');
         return { statusCode: 200, body: JSON.stringify(response.data) };
     })
     .catch(function (error) {
