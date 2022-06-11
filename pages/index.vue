38135e6bc68823762b75865071fb12a74eacccf0
Sandeep Kumar Yadav <sandeep240288@gmail.com>
2022-06-11 23:51:09 +0530

    Netlify CMS added content and static foders

--- pages/index.vue
+++ pages/index.vue
@@ -87,3 +87,12 @@
   </div>
 </template>
 
+<script>
+export default {
+  head() {
+    return {
+      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+    };
+  },
+};
+</script>
