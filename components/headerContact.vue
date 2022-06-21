bec33595d441e7949b617a6bfc4c7994e5dd2b5f
Sandeep Kumar Yadav <sandeep240288@gmail.com>
2022-06-21 12:51:16 +0530

    Telephone and whatsapp

--- components/headerContact.vue
+++ components/headerContact.vue
@@ -1,35 +1,35 @@
 <template>
-    <div class="bg-gray-800 text-gray-200 shadow-md w-full backdrop-blur sticky top-0 z-200">
-        <nav class="container pl-2 pr-6 py-2 mx-auto md:flex md:justify-between md:items-center">
-            <div class="flex justify-between items-center gap-4">
-                <div md:hidden>
-                    For Enquiries:
-                </div>
+  <div class="bg-gray-800 text-gray-200 shadow-md w-full backdrop-blur">
+    <nav class="container pl-2 pr-6 py-2 mx-auto md:flex md:justify-between md:items-center">
+      <div class="md:flex md:justify-between md:items-center gap-4">
+        <div>For Enquiries:</div>
 
-                <div>
-                    <span itemprop="telephone"><a href="tel:+918604719945">
-                            +918604719945</a></span>
-                </div>
-                <div> </div>
-                <div>
-                    <span itemprop="telephone"><a href="tel:+916307658884">
-                            +916307658884</a></span>
-                </div>
-                <div>
-                    <span itemprop="telephone">
-                        <a href="https://wa.me/918604719945" target="_blank">
-                            <img src="assets/image/whatsapp_logo.jpg" alt="" height="20" width="20">
-                        </a>
-                    </span>
-                </div>
-            </div>
+        <div>
+          <span itemprop="telephone">
+            <a href="tel:+918604719945">+918604719945</a>
+          </span>
+        </div>
+        <div></div>
+        <div>
+          <span itemprop="telephone">
+            <a href="tel:+916307658884">+916307658884</a>
+          </span>
+        </div>
+        <div>
+          <span itemprop="telephone">
+            <a href="https://wa.me/918604719945" target="_blank">
+              <img src="assets/image/whatsapp_logo.jpg" alt height="20" width="20" />
+            </a>
+          </span>
+        </div>
+      </div>
 
-
-            <div>
-                Email: <span itemprop="email"><a href="mailto:theindiandefenceacademy@gmail.com">
-                        theindiandefenceacademy@gmail.com</a></span>
-            </div>
-
-        </nav>
-    </div>
+      <div>
+        Email:
+        <span itemprop="email">
+          <a href="mailto:theindiandefenceacademy@gmail.com">theindiandefenceacademy@gmail.com</a>
+        </span>
+      </div>
+    </nav>
+  </div>
 </template>
